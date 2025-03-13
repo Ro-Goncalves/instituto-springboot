22f88e93c30309e2f8028a96473ec42798448d2d
rodrigo.goncalves <ro.go.calves@brconsorcios.com>
2025-03-13 08:16:52 -0300

    feat(brianbot): Atualizar as configurações de inicialização do Spring Boot

---
+++ brianbot/src/main/java/com/rgbrain/brianbot/BrianbotApplication.java
@@ -0,0 +1,13 @@
+package com.rgbrain.brianbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrianbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrianbotApplication.class, args);
+	}
+
+}
